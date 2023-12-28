4349dc2e8cacc83f9feb60afbebd41609524b101
VishnuWD <vishnuyadavhome@gmail.com>
2023-12-29 02:37:44 +0530

    first commit of srcrec

---
+++ main.js
@@ -0,0 +1,108 @@
+let stream = null,
+	audio = null,
+	mixedStream = null,
+	data = [], 
+	recorder = null
+	startButton = null,
+	stopButton = null,
+	downloadButton = null,
+	recordedVideo = null;
+
+async function setupStream () {
+	try {
+		stream = await navigator.mediaDevices.getDisplayMedia({
+			video: true
+		});
+
+		audio = await navigator.mediaDevices.getUserMedia({
+			audio: {
+				echoCancellation: true,
+				noiseSuppression: true,
+				sampleRate: 44100,
+			},
+		});
+
+		setupVideoFeedback();
+	} catch (err) {
+		console.error(err)
+	}
+}
+
+function setupVideoFeedback() {
+	if (stream) {
+		const video = document.querySelector('.video-feedback');
+		video.srcObject = stream;
+		video.play();
+	} else {
+		console.warn('No stream available');
+	}
+}
+
+async function startRecording () {
+	await setupStream();
+
+	if (stream && audio) {
+		mixedStream = new MediaStream([...stream.getTracks(), ...audio.getTracks()]);
+		recorder = new MediaRecorder(mixedStream);
+		recorder.ondataavailable = handleDataAvailable;
+		recorder.onstop = handleStop;
+		recorder.start(1000);
+	
+		startButton.disabled = true;
+		stopButton.disabled = false;
+	
+		console.log('Recording started');
+	} else {
+		console.warn('No stream available.');
+	}
+}
+
+function stopRecording () {
+	recorder.stop();
+
+	startButton.disabled = false;
+	stopButton.disabled = true;
+}
+
+function handleDataAvailable (e) {
+	data.push(e.data);
+}
+
+function handleStop (e) {
+	// 👇 Blob reads raw data and converts them into readablestream 
+	// (in this case it converts into video file) 
+	const blob = new Blob(data, { 'type' : 'video/mp4' });
+	data = [];
+
+	// 👇 converts blob data into downloadable urll 
+	downloadButton.href = URL.createObjectURL(blob);
+	downloadButton.download = 'video.mp4';
+	downloadButton.disabled = false;
+
+	recordedVideo.src = URL.createObjectURL(blob);
+	recordedVideo.load();
+	recordedVideo.onloadeddata = function() {
+		const rc = document.querySelector(".recorded-video-wrap");
+		rc.classList.remove("hidden");
+
+		// 👇 scrolls to the prewiew and download page
+		rc.scrollIntoView({ behavior: "smooth", block: "start" });
+
+		recordedVideo.play();
+	}
+
+	stream.getTracks().forEach((track) => track.stop());
+	audio.getTracks().forEach((track) => track.stop());
+
+	console.log('Record has been STOPPED');
+}
+
+window.addEventListener('load', () => {
+	startButton = document.querySelector('.start-recording');
+	stopButton = document.querySelector('.stop-recording');
+	downloadButton = document.querySelector('.download-video');
+	recordedVideo = document.querySelector('.recorded-video');
+
+	startButton.addEventListener('click', startRecording);
+	stopButton.addEventListener('click', stopRecording);
+})
